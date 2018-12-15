3cec3622280ebca800380481ae382279a75b829b
Martin Appleton <m.g.appleton@gmail.com>
2018-12-15 20:13:45 +1100

    Changed npm start to run react and gulp at the same time

--- gulpfile.js
+++ gulpfile.js
@@ -1,32 +1,21 @@
+const { src, dest, watch } = require('gulp');
 
-var gulp = require('gulp');
-//var uglify = require('gulp-uglify');
-var sass = require('gulp-sass')
+const sass = require('gulp-sass')
 
-
-
-gulp.task('scss', function() {
-
-    return gulp.src("./SCSS/martiBlog.scss")
-    .pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
-    .pipe(gulp.dest('src/css'));
-});
+function scss(cb) {
+    // place code for your default task here
+    return src('SCSS/martiBlog.scss')
+            .pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
+            .pipe(dest('./src'));
+    cb();
+  }
 
 
-
-// The default task (called when you run `gulp`)
-gulp.task('default', function() {
-  gulp.run('scss');
-
-  // Watch files and run tasks if they change
-//   gulp.watch('client/js/**', function(event) {
-//     gulp.run('scripts');
-//   });
-
- //For later 
-//   gulp.watch([
-    
-//   ], function(event) {
-//     gulp.run('copy');
-//   });
-});
+function defaultTask(cb) {
+    // place code for your default task here
+        scss(cb);
+        watch('SCSS/**/*.scss', scss);
+        cb();
+  }
+  
+  exports.default = defaultTask
